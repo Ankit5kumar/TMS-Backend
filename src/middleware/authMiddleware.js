a5bc558d3a403ea8c2dfc95568963a0c2f1c36bb
Ankit kumar <ak8068716@gmail.com>
2025-01-22 16:37:30 +0530

    secret key added

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -4,9 +4,9 @@ const verifyToken = (req, res, next) => {
     const token = req.headers['authorization']?.split(' ')[1];
     
     if (!token) return res.status(403).json({ message: 'Unauthorized' });
-    
-    jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
-        if (err) return res.status(401).json({ message: 'Unauthorized' });
+    // process.env.JWT_SECRET
+    jwt.verify(token, "secret_key", (err, decoded) => {
+        if (err) return res.status(401).json({ message: 'Unauthorizedd' });
         
         req.user = decoded; 
         next();
