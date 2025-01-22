c8122b2b4ed76cd39dc336e664a1773442a3b520
Ankit kumar <ak8068716@gmail.com>
2025-01-22 17:44:24 +0530

    new string added

--- src/config/db.js
+++ src/config/db.js
@@ -3,9 +3,10 @@ require('./env');
 // 
 const connectDB = async ()=>{
     try {
-        await mongoose.connect("mongodb+srv://ak8068716:Ankit5kumar@task-managment-system.1rpkw.mongodb.net/test",{
+        await mongoose.connect("mongodb+srv://ak8068716:Ankit5kumar@task-managment-system.1rpkw.mongodb.net/?retryWrites=true&w=majority&appName=Task-managment-system/test",{
             // useNewUrlParser:true,
             // useUnifiedTopology:true,
+           
         });
         console.log("MongoDB Connected");
     } catch (err) {
